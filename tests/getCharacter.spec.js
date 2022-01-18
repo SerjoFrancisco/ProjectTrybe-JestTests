e570a0044f4dc76310017772d9c4c89464af7eff
Sergio Francisco <spinheiro.lf@gmail.com>
2022-01-18 12:14:15 -0300

    testando os personagens

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,7 +43,41 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+  });
+  it('Retorna o objeto esperado', () => {
+    const arya = {
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'],
+    };
+    const brienne = {
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+    };
+    const melissandre = {
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+    };
+    expect(getCharacter('Arya')).toMatchObject(arya);
+    expect(getCharacter('Brienne')).toMatchObject(brienne);
+    expect(getCharacter('Melissandre')).toMatchObject(melissandre);
+  });
+  it('Não é `case sensitive`', () => {
+    const arya = {
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'],
+    };
+    expect(getCharacter('arya')).toMatchObject(arya);
+    expect(getCharacter('aRYa')).toMatchObject(arya);
+    expect(getCharacter('ArYa')).toMatchObject(arya);
+  });
+  it('não retorna nada fora da lista', () => {
+    expect(getCharacter()).toBeUndefined();
+    expect(getCharacter('bob')).toBeUndefined();
+  });
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
@@ -51,5 +85,5 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
     // Teste se os parâmetros não são Case Sensitive.
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
-  });
+  
 });
